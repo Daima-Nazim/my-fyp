b636ede930e01a686bafa6feb43958e016217024
Daima-Nazim <74188937+Daima-Nazim@users.noreply.github.com>
2020-12-22 11:39:22 +0500

    patient front end
    patient front end

---
+++ uploadfile.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class Patient_uploadfile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+
+    }
+    protected void btnsave_Click(object sender, EventArgs e)
+    {
+        using (AIDiseaseDetectionDBEntities db = new AIDiseaseDetectionDBEntities())
+        {
+            tblimage3 img = new tblimage3();
+            byte[] bytes;
+            using (BinaryReader br = new BinaryReader(flupload.PostedFile.InputStream))
+            {
+                bytes = br.ReadBytes(flupload.PostedFile.ContentLength);
+            }
+            img.image = bytes;
+            db.tblimage3.Add(img);
+            db.SaveChanges();
+            lblMsg.Text = "Image Uploaded Successfully";
+        }
+    }
+}
